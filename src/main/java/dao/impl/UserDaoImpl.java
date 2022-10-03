aada9894172b6667cb26fed3a662b6ec27a5d002
yeyeye <h2012497638@outlook.com>
2022-10-03 16:56:09 +0800

    添加了通过昵称找用户的方法（有bug） 创建新用户

--- src/main/java/dao/impl/UserDaoImpl.java
+++ src/main/java/dao/impl/UserDaoImpl.java
@@ -3,6 +3,7 @@ package dao.impl;
 import com.zz.core.SqlSession;
 import com.zz.core.SqlSessionFactory;
 import dao.UserDao;
+import pojo.po.Account;
 import pojo.po.User;
 import utils.DaoUtil;
 
@@ -15,6 +16,12 @@ public class UserDaoImpl implements UserDao {
         this.sqlSessionFactory = DaoUtil.getSqlSessionFactory();
     }
 
+    /**
+     * 通过用户id获取该用户
+     *
+     * @param userId 需要查找的用户id
+     * @return
+     */
     @Override
     public User selectUserById(int userId) {
         SqlSession sqlSession = sqlSessionFactory.openSession();
@@ -23,5 +30,61 @@ public class UserDaoImpl implements UserDao {
         List<Object> objectList = sqlSession.selectList("UserMapper.selectUserById", user);
         sqlSession.close();
         return objectList.size() == 0 ? null : (User) objectList.get(0);
+    }
+
+    /**
+     * 通过用户id获取该用户
+     *
+     * @param nickName 需要查找的用户昵称
+     * @return (有bug, 如果存在重名用户则返回查到的第一个)
+     */
+    @Override
+    public User selectUserByNickName(String nickName) {
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        User user = new User();
+        user.setNickName(nickName);
+        List<Object> objectList = sqlSession.selectList("UserMapper.selectUserByNickName", user);
+        sqlSession.close();
+        return objectList.size() == 0 ? null : (User) objectList.get(0);
+    }
+
+    /**
+     * 创建新用户
+     *
+     * @param number
+     * @param password
+     * @param nickName
+     * @return
+     */
+    @Override
+    public int createUserByNumber(String number, String password, String nickName) {
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        User user = new User();
+        user.setNickName(nickName);
+        //插入用户
+        int insert = sqlSession.insert("UserMapper.insertUserByNickName", user);
+        if (insert > 0) {
+            sqlSession.commit();
+        } else {
+            sqlSession.rollBack();
+            return insert;
+        }
+        //插入用户成功
+        //获取userId
+        user = selectUserByNickName(nickName);
+        int userId = user.getUserId();
+        //插入用户对应账号
+        Account account = new Account();
+        account.setNumber(number);
+        account.setPassword(password);
+        account.setUserId(userId);
+        insert = sqlSession.insert("AccountMapper.insertAccount", account);
+        if (insert > 0) {
+            sqlSession.commit();
+        } else {
+            sqlSession.rollBack();
+        }
+        sqlSession.close();
+        return insert;
     }
 }
