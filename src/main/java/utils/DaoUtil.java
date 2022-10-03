690f95fa1a3fe09c848e0e2b3ebf2d532ae0e544
yeyeye <h2012497638@outlook.com>
2022-10-03 16:55:09 +0800

    修改了获取sqlsessionfactory的方法

--- src/main/java/utils/DaoUtil.java
+++ src/main/java/utils/DaoUtil.java
@@ -4,16 +4,15 @@ import com.zz.core.SqlSessionFactory;
 import com.zz.core.SqlSessionFactoryBuilder;
 import com.zz.utils.Resources;
 
+import java.io.IOException;
 import java.io.InputStream;
 
 public class DaoUtil {
     private static final SqlSessionFactoryBuilder sqlSessionFactoryBuilder;
 
-    private static final InputStream configInputStream;
-
     static {
         sqlSessionFactoryBuilder = new SqlSessionFactoryBuilder();
-        configInputStream = Resources.getResourceAsStream("easydao-config.xml");
+
     }
 
     /**
@@ -22,6 +21,13 @@ public class DaoUtil {
      * @return SqlSessionFactory对象
      */
     public static SqlSessionFactory getSqlSessionFactory() {
-        return sqlSessionFactoryBuilder.build(configInputStream);
+        InputStream input = Resources.getResourceAsStream("easydao-config.xml");
+        SqlSessionFactory sqlSessionFactory = sqlSessionFactoryBuilder.build(input);
+        try {
+            input.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return sqlSessionFactory;
     }
 }
