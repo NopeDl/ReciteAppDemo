eac7ad2a1124b3717ac16b6566a7eced76cec37c
yeyeye <h2012497638@outlook.com>
2022-10-03 16:56:30 +0800

    添加创建新用户功能

--- src/main/java/service/UserService.java
+++ src/main/java/service/UserService.java
@@ -1,12 +1,25 @@
 package service;
 
+import jakarta.servlet.http.HttpServletRequest;
 import pojo.po.User;
+import pojo.vo.Message;
 
 public interface UserService {
     /**
      * 通过userId查找用户
+     *
      * @param userId
-     * @return 找得到返回User,否则返回fasle
+     * @return 找得到返回User, 否则返回fasle
      */
     User selectUserById(int userId);
+
+
+    /**
+     * 注册用户
+     *
+     * @return
+     */
+    Message<?> createUser(HttpServletRequest request);
+
+
 }
