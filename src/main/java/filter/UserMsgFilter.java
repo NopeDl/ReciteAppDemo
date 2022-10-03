d9c91ba30b77d3b62f7ec091e88c9cd141d77b1c
yeyeye <h2012497638@outlook.com>
2022-10-04 00:07:49 +0800

    javax->jakarta

--- src/main/java/filter/UserMsgFilter.java
+++ src/main/java/filter/UserMsgFilter.java
@@ -1,14 +1,13 @@
 package filter;
 
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletResponse;
 import service.UserService;
 import service.impl.UserServiceImpl;
 
-import javax.servlet.*;
-import javax.servlet.annotation.WebFilter;
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+
 //拦截用户获取信息
 @WebFilter("/UserMsg")
 public class UserMsgFilter implements Filter {
@@ -17,14 +16,14 @@ public class UserMsgFilter implements Filter {
 
     public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws ServletException, IOException {
         //调用查找cookie是否有userId
-        UserService userService=new UserServiceImpl();
-        String userId=userService.getCookie((jakarta.servlet.http.HttpServletRequest) req,"userId");
+        UserService userService = new UserServiceImpl();
+        String userId = userService.getCookie((jakarta.servlet.http.HttpServletRequest) req, "userId");
 
-        if(userId==null){
+        if (userId == null) {
             //此时没有userId,说明用户没有进行登录，跳转到登录页面
-            HttpServletResponse response= (HttpServletResponse) resp;
+            HttpServletResponse response = (HttpServletResponse) resp;
             response.sendRedirect("Login");
-        }else{
+        } else {
             //否则放行
             chain.doFilter(req, resp);
         }
