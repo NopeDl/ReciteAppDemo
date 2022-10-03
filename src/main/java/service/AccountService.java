6937e71b3c628d9515b49278babcdce22276742e
yeyeye <h2012497638@outlook.com>
2022-10-03 16:54:26 +0800

    将检测账户方法参数修改为request和response

--- src/main/java/service/AccountService.java
+++ src/main/java/service/AccountService.java
@@ -1,15 +1,14 @@
 package service;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import pojo.po.Account;
 import pojo.vo.Message;
 
 public interface AccountService {
     /**
      * 通过number和password查找账户
-     *
-     * @param number
-     * @param password
      * @return number和password对的上返回account，否则返回null
      */
-    Message checkAccount(String number, String password);
+    Message checkAccount(HttpServletRequest request, HttpServletResponse response);
 }
