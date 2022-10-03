93e3ab011702f7f739db6cf7840d5243f4446e27
isADucka <2867025942@qq.com>
2022-10-03 15:01:18 +0800

    查找用户资料

--- src/main/java/service/UserService.java
+++ src/main/java/service/UserService.java
@@ -1,12 +1,15 @@
 package service;
 
 import pojo.po.User;
+import pojo.vo.Message;
 
 public interface UserService {
     /**
-     * 通过userId查找用户
+     * 根据用户的id号查找该用户的个人资料
      * @param userId
-     * @return 找得到返回User,否则返回fasle
+     * @return 返回user
      */
-    User selectUserById(int userId);
+    User getMsgById(int userId);
+
+
 }
--- src/main/java/service/impl/UserServiceImpl.java
+++ src/main/java/service/impl/UserServiceImpl.java
@@ -3,15 +3,19 @@ package service.impl;
 import dao.UserDao;
 import dao.impl.UserDaoImpl;
 import pojo.po.User;
+import pojo.vo.Message;
 import service.UserService;
 
 public class UserServiceImpl implements UserService {
 
-    private UserDao UserDao = new UserDaoImpl();
+    private UserDao userDao = new UserDaoImpl();
+
 
     @Override
-    public User selectUserById(int userId) {
-        User user = UserDao.selectUserById(userId);
+    //通过userId来查找用户资料
+    public User getMsgById(int userId) {
+        User user = userDao.selectUserById(userId);
         return user;
+
     }
 }
