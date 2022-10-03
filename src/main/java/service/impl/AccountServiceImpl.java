eb006353806cdff5fe4f9e05fc1d17c20a8b4822
yeyeye <h2012497638@outlook.com>
2022-10-03 16:54:50 +0800

    验证成功后发送cookie给用户

--- src/main/java/service/impl/AccountServiceImpl.java
+++ src/main/java/service/impl/AccountServiceImpl.java
@@ -3,6 +3,9 @@ package service.impl;
 import dao.AccountDao;
 import dao.impl.AccountDaoImpl;
 import enums.MsgInf;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import pojo.po.Account;
 import pojo.vo.Message;
 import service.AccountService;
@@ -12,13 +15,11 @@ public class AccountServiceImpl implements AccountService {
 
     /**
      * 根据手机号和密码验证账户
-     *
-     * @param number
-     * @param password
-     * @return
      */
     @Override
-    public Message<?> checkAccount(String number, String password) {
+    public Message<?> checkAccount(HttpServletRequest request, HttpServletResponse response) {
+        String number = request.getParameter("phone");
+        String password = request.getParameter("password");
         Account account = accountDao.selectAccount(number, password);
         Message<Object> msg;
         if (account == null) {
@@ -27,6 +28,9 @@ public class AccountServiceImpl implements AccountService {
         } else {
             //验证成功
             msg = new Message<>();
+            //发送userId cookie给用户
+            Cookie cookie = new Cookie("userId", account.getUserId() + "");
+            response.addCookie(cookie);
         }
         return msg;
     }
