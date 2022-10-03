ee3dff7b4cad9f19680579f98fd04e92eeebcc75
yeyeye <h2012497638@outlook.com>
2022-10-03 16:55:57 +0800

    添加了通过昵称找用户的方法（有bug） 创建新用户

--- src/main/java/dao/UserDao.java
+++ src/main/java/dao/UserDao.java
@@ -5,9 +5,28 @@ import pojo.po.User;
 public interface UserDao {
     /**
      * 通过用户id获取该用户
+     *
      * @param userId 需要查找的用户id
      * @return 存在则返回该用户pojo，不存在返回null
      */
     User selectUserById(int userId);
+
+    /**
+     * 通过用户id获取该用户
+     *
+     * @param nickName 需要查找的用户昵称
+     * @return 存在则返回该用户pojo，不存在返回null
+     */
+    User selectUserByNickName(String nickName);
+
+    /**
+     * 根据手机号创建新用户
+     *
+     * @param number
+     * @param password
+     * @param nickName
+     * @return
+     */
+    int createUserByNumber(String number, String password, String nickName);
 
 }
