05c7345942647f123bcdd3d0439fa1b05c642383
isADucka <2867025942@qq.com>
2022-10-04 17:48:07 +0800

    修改用户资料

--- src/main/java/controller/UserController.java
+++ src/main/java/controller/UserController.java
@@ -8,6 +8,7 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import pojo.po.User;
 import pojo.vo.Message;
 import service.AccountService;
 import service.UserService;
@@ -16,7 +17,12 @@ import service.impl.UserServiceImpl;
 import utils.ResponseUtil;
 import utils.StringUtil;
 
+import javax.xml.crypto.Data;
 import java.io.IOException;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * 执行用户相关操作
@@ -45,10 +51,40 @@ public class UserController extends HttpServlet {
         } else if ("ChangePswd".equals(requestURI)) {
             //修改密码
             msg = accountService.changePassword(request);
-        } else {
+        } else if("reMessage".equals(requestURI)){
+            //用户修改个人资料
+            //获取用户修改后的内容
+            String phone = request.getParameter("phone");
+            String nickName = request.getParameter("nickName");
+            String sex = request.getParameter("sex");
+            String time =request.getParameter("birthday");
+            //将生日转化为data
+            DateFormat fmt =new SimpleDateFormat("yyyy-MM-dd");
+            Date birthday = null;
+            try {
+                birthday = fmt.parse(time);
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+
+            Integer points = Integer.parseInt(request.getParameter("points"));
+            String imagePath = request.getParameter("imagePath");
+            Integer cityId = Integer.parseInt(request.getParameter("cityId"));
+            String schoolId = request.getParameter("schoolId");
+
+            //获取userId
+            Integer userId = accountService.getIdByNumber(phone);
+            //封装用户修改后的资料
+            User user=new User(userId,nickName,sex,birthday,points,imagePath,cityId,schoolId);
+            //更改修改后的数据
+            msg = userService.ReMsgById(user);
+        }else {
             msg = new Message<>(MsgInf.NOT_FOUND);
         }
         //发送响应消息体
         ResponseUtil.send(response, msg);
+
+
+
     }
 }
