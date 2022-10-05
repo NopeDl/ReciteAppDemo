ed6055454fa4dee5dbc77667c583e5819fac706b
yeyeye <h2012497638@outlook.com>
2022-10-05 23:10:19 +0800

    String工具类优化

--- src/main/java/utils/StringUtil.java
+++ src/main/java/utils/StringUtil.java
@@ -3,11 +3,11 @@ package utils;
 public class StringUtil {
     /**
      * 获取uri
+     *
      * @param url 需要解析的url
      * @return uri
      */
     public static String parseURI(String url) {
-        String[] split = url.split("/");
-        return split[split.length - 1];
+        return url.substring(url.lastIndexOf("/") + 1);
     }
 }
