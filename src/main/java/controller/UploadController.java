505788ef0a11b88b581f8e0478b7d27572b646de
yeyeye <h2012497638@outlook.com>
2022-10-05 23:12:26 +0800

    上传文件功能

---
+++ src/main/java/controller/UploadController.java
@@ -0,0 +1,37 @@
+package controller;
+
+import enums.MsgInf;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.MultipartConfig;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import pojo.vo.Message;
+import service.UserService;
+import service.impl.UserServiceImpl;
+import utils.ResponseUtil;
+import utils.StringUtil;
+
+import java.io.IOException;
+
+
+@WebServlet("/upload/*")
+@MultipartConfig
+public class UploadController extends HttpServlet {
+    private UserService userService = new UserServiceImpl();
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String uri = StringUtil.parseURI(request.getRequestURI());
+        Message<?> msg;
+        if ("file".equals(uri)) {
+            //上传文件
+            msg = userService.setFileById(request);
+        } else {
+            msg = new Message<>(MsgInf.NOT_FOUND);
+        }
+        ResponseUtil.send(response, msg);
+    }
+}
